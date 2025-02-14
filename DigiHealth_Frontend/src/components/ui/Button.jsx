e1fbec4945adce0fbb309628a231aab49980d6c8
Aniket <136266153+Anikerithmic@users.noreply.github.com>
2025-02-14 15:58:37 +0530

    :sparkles:Feat:Add Admin Dashboard and ui components (#2)

---
+++ DigiHealth_Frontend/src/components/ui/Button.jsx
@@ -0,0 +1,10 @@
+const Button = ({ children, className, onClick }) => {
+    return (
+      <button onClick={onClick} className={`px-4 py-2 rounded-md ${className}`}>
+        {children}
+      </button>
+    );
+  };
+  
+  export default Button;
+  
