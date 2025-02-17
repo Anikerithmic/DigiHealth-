3a67b945a8f81511dd9c697b3237e2bf33a2e409
Aniket <136266153+Anikerithmic@users.noreply.github.com>
2025-02-17 17:13:19 +0530

    Feat/patient dashboard (#3)
    * :sparkles:Feat:Add Admin Dashboard and ui components
    
    * :sparkles:Feat:Add Patient Dashboard and Booking Modal

---
+++ DigiHealth_Frontend/src/components/ui/AppointmentModal.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Button from "./Button";
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+      <div className="bg-gray-900 p-6 rounded-lg shadow-lg w-96 relative">
+        <button
+          className="absolute top-4 right-4 text-gray-400 hover:text-white"
+          onClick={onClose}
+        >
+          &times;
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
