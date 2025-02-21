9f42eb3b8c99d981e7186716d468a28ca5447783
Aniket <136266153+Anikerithmic@users.noreply.github.com>
2025-02-21 17:28:14 +0530

    Feat/doctor dashboard (#4)  :sparkles:Feat:Add Doctor Dashboard
    * :sparkles:Feat:Add Admin Dashboard and ui components
    
    * :sparkles:Feat:Add Patient Dashboard and Booking Modal
    
    * :sparkles:Feat:Add Doctor Dashboard

--- DigiHealth_Frontend/src/App.jsx
+++ DigiHealth_Frontend/src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminDashboard from './components/AdminDashboard';
 import PatientDashboard from './components/PatientDashboard';
 import SuccessPage from "./components/SuccessPage";
-
+import DoctorDashboard from "./components/DoctorDashboard";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +17,7 @@ function App() {
       <Route path="/admin-dashboard" element={<AdminDashboard />} />
       <Route path="/patient-dashboard" element={<PatientDashboard />} />
       <Route path="/success" element={<SuccessPage />} />
+      <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
     </Routes>
   </Router>
   )
